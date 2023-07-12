fa4b4f2bfc0f57d0927b758eeb66218c9ad720e7
Carlos Martin Quispe Bartolo <104172383+tripalidex@users.noreply.github.com>
2023-07-11 21:33:48 -0500

    courseinfo-v1

---
+++ part1-react/courseinfo/src/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Title = ({course}) => {
+  return <h1>{course}</h1>
+}
+/*
+const Title = (props) => {
+  //desestructuracion
+  const {course} = props//const course = props.course
+  
+  return <h1>{course}</h1>
+}
+*/
+/*
+//one liner (pues solo devuelve un solo elemento)
+const Title = ({course}) => <h1>{course}</h1>
+*/
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Title course={course}/>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
